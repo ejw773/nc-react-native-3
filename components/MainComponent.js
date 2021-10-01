c170f16d9fcb15d8bb2e6519d688501d78c208b1
Elijah Wilcott <ejw773@gmail.com>
2021-10-01 16:21:23 -0400

    moves navigation from App.js to  MainComponent.js

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,20 +1,54 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
 
-const Main = ({ navigation }) => {
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Directory from './DirectoryComponent'; 
+import CampsiteInfo from './CampsiteInfoComponent'
+import Home from './HomeComponent';
+
+const Stack = createNativeStackNavigator();
+
+const Main = () => {
     return (
-      <View style={styles.viewStyle}>
-        <Text>Home Screen</Text>
-      </View>
+      <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName="Directory"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#5637DD'
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            color: '#fff'
+          }
+        }}
+      >
+        <Stack.Screen 
+          name='Home'
+          component={Home}
+          options={{
+
+          }}
+        />
+        <Stack.Screen 
+          name='Directory' 
+          component={Directory}
+          options={{
+            title: 'Directory Screen'
+          }}
+        />
+        <Stack.Screen 
+          name="Campsite" 
+          component={CampsiteInfo} 
+          options={{
+            title: 'Campsite',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
     );
   }
-  
-const styles = StyleSheet.create({
-    viewStyle: {
-        flex: 1, 
-        alignItems: 'center', 
-        justifyContent: 'center'
-    }
-})
 
 export default Main;
