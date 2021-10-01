1743919ee3f8f3fa60a968ff1e038e31c4abd4bb
Elijah Wilcott <ejw773@gmail.com>
2021-10-01 17:09:02 -0400

    adds drawer navigation

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,52 +2,68 @@ import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import Directory from './DirectoryComponent'; 
 import CampsiteInfo from './CampsiteInfoComponent'
 import Home from './HomeComponent';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const drawerNavigator = () => {
+  return (
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Directory" component={Directory} />
+      </Drawer.Navigator>
+  )
+}
 
 const Main = () => {
     return (
       <NavigationContainer>
-      <Stack.Navigator 
-        initialRouteName="Directory"
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#5637DD'
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-            color: '#fff'
-          }
-        }}
-      >
-        <Stack.Screen 
-          name='Home'
-          component={Home}
-          options={{
-
+        <Stack.Navigator 
+          initialRouteName="Drawer"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              color: '#fff'
+            }
           }}
-        />
-        <Stack.Screen 
-          name='Directory' 
-          component={Directory}
-          options={{
-            title: 'Directory Screen'
-          }}
-        />
-        <Stack.Screen 
-          name="Campsite" 
-          component={CampsiteInfo} 
-          options={{
-            title: 'Campsite',
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+          >
+          <Stack.Screen 
+            name='Drawer'
+            component={drawerNavigator}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen 
+            name='Home'
+            component={Home}
+            options={{
+              title: 'Home'
+            }}
+          />
+          <Stack.Screen 
+            name='Directory' 
+            component={Directory}
+            options={{
+              title: 'Directory Screen'
+            }}
+          />
+          <Stack.Screen 
+            name="Campsite" 
+            component={CampsiteInfo} 
+            options={{
+              title: 'Campsite',
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 
