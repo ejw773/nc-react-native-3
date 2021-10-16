0d3e1ae306a40578e1337d3e96451fcca192d9ab
Elijah Wilcott <ejw773@gmail.com>
2021-10-15 21:40:44 -0400

    animations

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useSelector, useDispatch } from  'react-redux';
 import { fetchCampsites } from '../redux/campsitesSlice';
 import { fetchPartners } from '../redux/partnersSlice';
@@ -35,30 +35,45 @@ const RenderItem = (props) => {
 }
 
 const Home = () => {
-    const [scaleValue, setScaleValue] = useState(new Animated.Value(0))
+    // const [scaleValue, setScaleValue] = useState(new Animated.Value(0))
     const campsites = useSelector((state) => state.campsites)
     const partners = useSelector((state) => state.partners)
     const promotions = useSelector((state) => state.promotions)
     const dispatch = useDispatch();
-    const animate = () => {
-        Animated.timing(
-            setScaleValue({
-                toValue: 1,
-                duration: 1500,
-                useNativeDriver: true
-            })
-        ).start();
+    // const animate = () => {
+    //     Animated.timing(
+    //         setScaleValue({
+    //             toValue: 1,
+    //             duration: 1500,
+    //             useNativeDriver: true
+    //         })
+    //     ).start();
+    // }
+
+    const fadeAnim = useRef(new Animated.Value(0)).current;
+
+    const fadeIn = () => {
+        Animated.timing(fadeAnim, {
+            toValue: 1,
+            duration: 1500,
+            useNativeDriver: true
+        }).start();
     }
+
 
     useEffect(() => {
         dispatch(fetchCampsites())
         dispatch(fetchPartners())
         dispatch(fetchPromotions())
-        //animate()
     }, [dispatch])
     
+    useEffect(() => {
+        console.log('fade in')
+        fadeIn();
+    })
+
     return (
-        <ScrollView>
+        <Animated.ScrollView>
             <RenderItem
                 item={campsites.campsites.filter(campsite => campsite.featured)[0]}
                 status={campsites.status}
@@ -77,13 +92,9 @@ const Home = () => {
                 errMess={partners.errMess}
                 itemType='Partners'
             />
-        </ScrollView>
+        </Animated.ScrollView>
     )
 }
-
-// Home.navigationOptions = {
-//     title: 'Home'
-// }
 
 const styles = StyleSheet.create({
     textStyle: {
